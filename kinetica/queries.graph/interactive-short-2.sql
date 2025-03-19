bfa72e09e65176bf97e2bd0e2696f84bf924cc99
douggrosvenor <dgrosvenor@kinetica.com>
2025-03-19 16:06:34 -0400

    Optimizing (will revisit to see if we don't need to do the forced MVs)

--- kinetica/queries.graph/interactive-short-2.sql
+++ kinetica/queries.graph/interactive-short-2.sql
@@ -23,9 +23,8 @@ RETURN
 ORDER BY messageCreationDate DESC, messageId ASC
 */
 
-WITH
-q1 as
---CREATE OR REPLACE TABLE ldbc._2_q1 as 
+
+CREATE OR REPLACE TABLE ldbc._2_q1 as 
 (
     SELECT s.*, m_creationdate
     FROM query_graph( graph => 'ldbc.graph', 
@@ -40,17 +39,14 @@ q1 as
     on s.QUERY_NODE_NAME_TARGET = CHAR64(CONCAT(t.m_messageid,'_m')) 
     ORDER by m_creationdate DESC
     LIMIT 10
-)
-,q2 as
---;CREATE OR REPLACE TABLE ldbc._2_q2 as 
+);
+CREATE OR REPLACE TABLE ldbc._2_q2 as 
 (
     /* KI_HINT_NO_PARALLEL_EXECUTION */ 
     SELECT *, QUERY_NODE_NAME_SOURCE as joinKey
     FROM query_graph ( graph => 'ldbc.graph', 
         queries => input_tables (
-            (SELECT q1.QUERY_NODE_NAME_TARGET AS NODE_NAME FROM 
-            --ldbc._2_q1 
-            q1)
+            (SELECT q1.QUERY_NODE_NAME_TARGET AS NODE_NAME FROM ldbc._2_q1 q1)
             ,(SELECT 1 AS HOP_ID, 'replyOf' AS EDGE_LABEL)
             ,(SELECT 2 AS HOP_ID, 'replyOf' AS EDGE_LABEL)
             ,(SELECT 3 AS HOP_ID, 'replyOf' AS EDGE_LABEL)
@@ -66,20 +62,17 @@ q1 as
         rings => 10, options => kv_pairs(find_common_labels = 'true', result_table_index = '2') 
     )
     UNION ALL
-    -- Get all messages that are posts, 0 hops
+    -- Get all 0 hop post messages
     SELECT *, QUERY_NODE_NAME_TARGET as joinKey
     FROM query_graph ( graph => 'ldbc.graph', 
         queries => input_tables (
-            (SELECT q1.QUERY_NODE_NAME_TARGET AS NODE_NAME FROM 
-            --ldbc._2_q1 
-            q1)
+            (SELECT q1.QUERY_NODE_NAME_TARGET AS NODE_NAME FROM ldbc._2_q1 q1)
         ),
         rings => 0, options => kv_pairs(find_common_labels = 'true', result_table_index = '2') 
     )
     WHERE LABELS = 'post'
-)
-,q3 as 
---;CREATE OR REPLACE TABLE ldbc._2_q3 as 
+);
+CREATE OR REPLACE TABLE ldbc._2_q3 as 
 (
     SELECT DISTINCT *
     FROM query_graph ( graph => 'ldbc.graph', 
@@ -89,24 +82,41 @@ q1 as
         ),
         rings => 1, options => kv_pairs(find_common_labels = 'true', result_table_index = '2') 
     ) s
-)
---; 
+); 
 
-SELECT 
+-- Do this to force replication in later steps
+CREATE OR REPLACE Materialized view ldbc._2_q4mv as (
+    SELECT 
+    p.p_personid
+    ,p.p_firstname
+    ,p.p_lastname
+    ,q2.joinkey
+FROM ldbc._2_q2 q2
+INNER JOIN ldbc._2_q3 q3 ON q2.QUERY_NODE_NAME_TARGET = q3.QUERY_NODE_NAME_SOURCE
+INNER JOIN ldbc.person p on q3.QUERY_NODE_NAME_TARGET = p.p_personid
+);
+
+-- Do this to force replication in later steps
+CREATE OR REPLACE Materialized view ldbc._2_q5mv as (
+    SELECT 
     m2.m_messageid
     ,coalesce(m2.m_content,m2.m_ps_imagefile) as m_content
     ,m2.m_creationdate
+    ,p_personid
+    ,p_firstname
+    ,p_lastname
+FROM ldbc._2_q4mv _2_q4mv
+INNER JOIN ldbc.messages m2 on _2_q4mv.joinkey = CHAR64(CONCAT(m2.m_messageid,'_m'))
+);
+
+SELECT 
+    _2_q5mv.m_messageid
+    ,_2_q5mv.m_content
+    ,_2_q5mv.m_creationdate
     ,m3.m_messageid as postid
-    ,p.p_personid
-    ,p.p_firstname
-    ,p.p_lastname
-FROM 
---ldbc._2_q2 
-q2
-INNER JOIN 
---ldbc._2_q3 
-q3 ON q2.QUERY_NODE_NAME_TARGET = q3.QUERY_NODE_NAME_SOURCE
-INNER JOIN ldbc.person p on q3.QUERY_NODE_NAME_TARGET = p.p_personid
-INNER JOIN ldbc.messages m2 on q2.joinKey = CHAR64(CONCAT(m2.m_messageid,'_m'))
-INNER JOIN ldbc.messages m3 on q2.QUERY_NODE_NAME_TARGET = CHAR64(CONCAT(m3.m_messageid,'_m'))
+    ,_2_q5mv.p_personid
+    ,_2_q5mv.p_firstname
+    ,_2_q5mv.p_lastname
+FROM ldbc.messages m3
+INNER JOIN ldbc._2_q5mv _2_q5mv ON m3.m_messageid = _2_q5mv.m_messageid
 ORDER BY m_creationdate DESC, m_messageid DESC
